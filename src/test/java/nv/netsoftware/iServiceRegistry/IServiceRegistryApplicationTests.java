1d0e95958c2ca29103d3853d23d6008035d93461
sqxsss <1514713140@qq.com>
2023-07-05 20:58:52 -0400

    initialize iservice registry

---
+++ src/test/java/nv/netsoftware/iServiceRegistry/IServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package nv.netsoftware.iServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
